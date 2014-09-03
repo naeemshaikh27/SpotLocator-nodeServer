0185abc4d9efb15b3dd52e659e3cdc2601c0da5f
naeem shaikh <naeemshaikh27@gmail.com>
2014-09-03 14:17:36 +0530

    zsdf

--- index.js
+++ index.js
@@ -156,7 +156,14 @@ app.post('/signup', function(req, res, next){
 	
   };
 	  
-	   var connection =  mysql.createConnection(obj);
+	   var connection =  mysql.createConnection({
+  		 host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+		   port     : process.env.OPENSHIFT_MYSQL_DB_PORT,
+         user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+         password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+         database : process.env.OPENSHIFT_GEAR_NAME
+	
+  });
 	connection.connect();
 	
 	
